e467c3700fd9ffccaf4a22e3f3a6fc6bcd8a5e1a
pks2906 <pratik1971sinha@gmail.com>
2024-09-01 03:12:05 +0530

    end points completed

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
 import GoogleProvider from "next-auth/providers/google";
 import NextAuth from "next-auth"
+import { prismaClient } from "@/app/lib/db";
 
 const handler = NextAuth({
 
@@ -8,7 +9,26 @@ const handler = NextAuth({
           clientId: process.env.GOOGLE_CLIENT_ID ?? "",
           clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
         })
-      ]
+      ],
+      callbacks: {
+        async signIn(params){
+          if (!params.user.email){
+            return false;
+          }
+          try{
+          await prismaClient.user.create({
+            data: {
+              email: params.user.email,
+              provider: "Google"
+            }
+
+          })
+        } catch(e) {
+           
+        }
+       return true;
+    }
+  }
 })
 
 export { handler as GET, handler as POST }
