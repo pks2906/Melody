9edc890d14a0733c142ad4b5887ddb6cabb8d9c8
pks2906 <pratik1971sinha@gmail.com>
2024-08-31 16:19:51 +0530

    logout status

--- app/compnents/Appbar.tsx
+++ app/compnents/Appbar.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import { signIn } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export function Appbar(){
+    const session = useSession();
     return <div>
         <div className="flex justify-between">
             <div>
@@ -11,7 +12,8 @@ export function Appbar(){
 
             </div>
             <div>
-                <button className="m-2 p-2 bg-blue-400" onClick={() => signIn()}>SignIn</button>
+                {session.data?.user && <button className="m-2 p-2 bg-blue-400" onClick={() => signOut()}>Logout</button>}
+                {!session.data?.user && <button className="m-2 p-2 bg-blue-400" onClick={() => signIn()}>SignIn</button>}
             </div>
         </div>
     </div>
